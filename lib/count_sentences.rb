09d1b7dcb34b070767b9d00b56409b4d77bdcc34
paulleerentveld <paul@veldnet.com.au>
2022-06-28 18:26:50 +1000

    lab done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
 
+    self.end_with?("?")
+
   end
 
   def exclamation?
 
+    self.end_with?("!")
+
   end
 
   def count_sentences
+
+    self.split(/\.|\?|\!/).delete_if {|item| item.size < 2}.count
+    #if there is more than one !! etc it returns a sentance  as "!" delete_if removes these instances
+    
 
   end
 end
